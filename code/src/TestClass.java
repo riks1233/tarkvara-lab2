e625aa1630130a9a512c68527bf23a40f382a0c9
Richardas <richardas.kersis@gmail.com>
2019-10-21 21:54:44 +0300

    test driven development 1 - failed tests

--- code/src/TestClass.java
+++
@@ -1,5 +0,0 @@
-public class TestClass {
-    public static int add(int a, int b) {
-        return a + b;
-    }
-}
---
+++ code/src/tdd_1_1/FileRenamer.java
@@ -0,0 +1,15 @@
+package tdd_1_1;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.util.Scanner;
+
+public class FileRenamer {
+
+    public static void renameAllInCurrentDirectory(String what, String to) throws IOException {
+
+    }
+}
+
